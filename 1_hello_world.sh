443cf3717d3820985631046be635e403daa2d7ef
W <96271708+FryLoVI@users.noreply.github.com>
2022-09-06 15:37:36 -0400

    Update 1_hello_world.sh

--- 1_hello_world.sh
+++ 1_hello_world.sh
@@ -3,7 +3,7 @@ Utilizing: vim
 sh, also known as Bourne Shell, is a command programming language for UNIX-like systems
 sh can take input from either a keyboard or a file
 ---
-Lines 17-19 into vim (insert mode) 
+Lines 21-23 into vim (insert mode) 
 To save a file in Vim / vi, press Esc key, type :w and hit Enter key.
 One can save a file and quit vim / Vi by pressing Esc key, type :x and hit Enter key.
 ---
